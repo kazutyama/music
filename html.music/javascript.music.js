f41721972370fd8066079bc4a230b86228ea3656
kazutyama <93856262+kazutyama@users.noreply.github.com>
2021-11-19 20:58:07 +0900

    Update javascript.music.js

--- html.music/javascript.music.js
+++ html.music/javascript.music.js
@@ -5,34 +5,48 @@ var music = [
     {link:"https://www.youtube.com/embed/E2Snh2NZHhk?list=PLUqo_wHHOCaxHMOjkXhe90hQhvb2hhIxM" ,text:"ODDISEE - OWN APPEAL (Official Video)"}
 ];
 
+/*const links = music.map((link) => {
+    return link.link;
+});
+links.forEach((likns) => {
+    let music_link = document.createElement('iframe');
+    music_link.src = likns;
+    music_link.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" ;
+    music_link.classList.add("class");
+    console.log(music_link);
+    document.getElementById("sample").appendChild(music_link);
+});*/
+
+
 for(let i = 0;i < music.length; i++) {
+   
+    
+    let article = document.createElement("article");
+    article.setAttribute("id",i);
+
+    let musicId = i;
+
+    document.getElementById("content").appendChild(article); 
+
     let music_text = document.createElement('p');
     music_text.textContent = music[i].text;
-    document.getElementById("content").appendChild(music_text); 
+    document.getElementById(musicId).appendChild(music_text); 
+    
+    //let count = music[i].link;
+
+    /*let music_link = document.createElement('iframe');
+    music_link.textContent = music[i].link;
+    document.getElementById("content").appendChild(music_link); */
     
     
-    //let music_link = document.createElement('iframe');
-    //music_link.textContent = music[i].link;
-    //document.getElementById("content").appendChild(music_link); 
-    
+    //実践版その二
     
     let music_link = document.createElement('iframe');
     music_link.src = music[i].link;
     music_link.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" ;
-    music_link.classList.add("class");
-    console.log(music_link);
-    document.getElementById("sample").appendChild(music_link); 
+    //music_link.classList.add("class");
+    document.getElementById(musicId).appendChild(music_link);
+    //article.innerHTML = music_link;
+
+    //document.getElementById("content").appendChild(article); 
 }
-
-//music_link.setAttribute("href");
-    //music_link.setAttribute(music[i].link);
-    
-    //let music_link = document.createElement('a');
-    //music_link.href = music[i].link;
-    //console.log(music_link);
-    //document.getElementById("sample").parentNode.append(music_link); 
-
-    //<iframe width="300" height="150" src="https://www.youtube.com/embed/eRf82JFCYpQ?list=PLUqo_wHHOCaxHMOjkXhe90hQhvb2hhIxM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  　//<iframe width="1280" height="720" src="https://www.youtube.com/embed/2uLjj81rPXI?list=PLUqo_wHHOCaxHMOjkXhe90hQhvb2hhIxM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    //<iframe width="1280" height="720" src="https://www.youtube.com/embed/jD-2gViW1bA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    //<iframe width="1280" height="720" src="https://www.youtube.com/embed/E2Snh2NZHhk?list=PLUqo_wHHOCaxHMOjkXhe90hQhvb2hhIxM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
